589e84293909cf3b96f1f51a596d7789fa55a998
Tom Canham <tom.canham@ctl.io>
2017-05-25 07:03:42 -0700

    Typo

--- src/scripts/pages/welcomePage.js
+++ src/scripts/pages/welcomePage.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 const WelcomePage = (props) => <div>
   <h1>Welcome to Small Practice</h1>
-  <h3>The premier practice management software for small- to medium-sized law paractices</h3>
+  <h3>The premier practice management software for small- to medium-sized law practices</h3>
   <p>Want to take a tour? Click <a>here</a>.</p>
   <p>Interested in joining? Click <a>here</a> to begin your free one month trial!</p>
   <h5 className="motd">{props.motd}</h5>
